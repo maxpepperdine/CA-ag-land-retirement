4c20dcd2789ba0c0e11847784622a8ba9df3a6b5
maxpepperdine <mpepperdine@ucsb.edu>
2026-03-02 10:49:52 -0800

    minor edits

--- scripts/archive/FallowFoxes_F25/8_blm.R
+++ scripts/archive/FallowFoxes_F25/8_blm.R
@@ -3,28 +3,20 @@ rm(list = ls())
 
 library(tidyverse)
 library(prioritizr)
-#library(prioritizrdata)
 library(sf)
 library(tmap)
 library(terra)
 library(rmapshaper)
 library(fasterize)
 
-# source("Scripts/0_startup/0_2_functions.R")
-
-
-# tmap_mode("view")
-# tmap_options(check.and.fix = TRUE)
-
-
 
 # Read In Data ------------------------------------------------------------
 
-
+# Land IQ fields with habitat area extracted to each field
 habitatExtract <- read_sf(here("data/intermediate/7_foxExtraction/kernExtractions/kernExtractions.shp")) %>% 
   st_make_valid()
 
-
+# create raster template based on output from 7_habitatExtraction
 rast_template <- rast(ext = ext(habitatExtract), 
                       resolution = 30, 
                       crs = st_crs(habitatExtract)$wkt) # wkt -> well-known text of the projection definition
@@ -32,20 +24,17 @@ rast_template <- rast(ext = ext(habitatExtract),
 
 
 
-# Rasterize Plots ---------------------------------------------------------
+# Rasterize & re-vectorize plots ---------------------------------------------------------
 
-
-# Rasterize
-
+# Rasterize 
 fieldRast <- habitatExtract %>% 
-  rasterize(rast_template, field = "id", touches = TRUE, fun = min)
+  rasterize(rast_template, 
+            field = "id", 
+            touches = TRUE,  # ensure all pixels that touch the field are included in the rasterization
+            fun = min)       # assign the minimum id value to each pixel (in case of overlapping fields)
 
 
-
-
-# Re-Vectorize ------------------------------------------------------------
-
-
+# Re-Vectorize
 reVector <- fieldRast %>% 
   as.polygons() %>% 
   st_as_sf()
@@ -55,19 +44,22 @@ reVector <- fieldRast %>%
 # Remove fields dropped from rasterization --------------------------------
 
 
+# some fields might be lost in rasterization due to narrowness of shape
+# crop these from the collection
+
 # ID of missing fields
 missing <- habitatExtract %>% dplyr::pull(id) %>% 
   setdiff(reVector %>% dplyr::pull(id))
 
-
+# subset habitatExtract to just the missing fields
 missingPolys <- habitatExtract %>%
   filter(id %in% missing)
 
-
-
+# list of missing field IDs
 badList <- missingPolys %>% 
   pull(id)
 
+# filter habitatExtract to remove missing fields
 habitatExtract2 <- habitatExtract %>% 
   filter(!(id %in% badList))
 
